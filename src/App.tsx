f8156115cecb5bd974dc455995eeeee3621d3f99
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-07-26 07:27:42 +0100

    fetched used effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {useEffect} from "react"
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const [error, setError] = useState({})
 
   useEffect(()=>{
     fetch(`https://jsonplaceholder.typicode.com/todos`)
     .then(response => response.json())
-    .then(res => console.log (res))
+    .then(res => setTodos(res.slice(0,10)))
+    .catch(err => setError(err))
   }, [])
+
+
+  // we want just 10 records so lets apply an array method
+  // 1st element in the array anf the last element which is not included
+
+  // install it in the state 
+  // error handling using cacth 
   return (
     <div className="App">
    
