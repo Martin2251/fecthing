6b122b3b27d343b96cd7b18d9dd4be0b5cf58889
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-07-26 07:19:23 +0100

    displaying data to console

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useEffect} from "react"
 
 function App() {
+
+  useEffect(()=>{
+    fetch(`https://jsonplaceholder.typicode.com/todos`)
+    .then(response => response.json())
+    .then(res => console.log (res))
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
     </div>
   );
 }
